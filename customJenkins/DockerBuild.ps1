59d2b334b4bd92e6ea29f5a2b173944e831ed706
burnlife <burnlife@qq.com>
2025-04-10 17:06:29 +0800

    Daily backup: 2025-04-10 17:06:29

--- customJenkins/DockerBuild.ps1
+++ customJenkins/DockerBuild.ps1
@@ -1,7 +1,7 @@
 # 构建镜像
-docker build -t my-custom-jenkins .
+docker build -t jenkins/jenkins .
 docker run -d --name jenkins --restart=on-failure `
   -e JAVA_OPTS="-Duser.timezone=Asia/Shanghai" -e TZ="Asia/Shanghai" `
   -v jenkins_home:/var/jenkins_home `
   -p 8080:8080 -p 50000:50000 `
-  my-custom-jenkins
+  jenkins/jenkins
